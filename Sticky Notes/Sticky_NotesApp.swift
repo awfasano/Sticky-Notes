7c294b17cf784663fdd871f0f3711be5dbd976f9
Anthony Fasano <awfasano@gmail.com>
2024-03-01 15:46:47 -0500

    Initial Commit

---
+++ Sticky Notes/Sticky_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sticky_NotesApp.swift
+//  Sticky Notes
+//
+//  Created by Anthony Fasano on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Sticky_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
